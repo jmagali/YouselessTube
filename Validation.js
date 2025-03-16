e8588a29cae88e23a40105609e4f0824f1ef9ebb
JM <jmagali.dev@gmail.com>
2025-03-16 10:48:26 -0400

    Update Validation.js

--- Validation.js
+++ Validation.js
@@ -1,5 +1,5 @@
 const form = document.getElementById('form')
-const firstname_input = document.getElementById('firstname-input')
+const fullName_input = document.getElementById('fullName-input')
 const username_input = document.getElementById('username-input')
 const password_input = document.getElementById('password-input')
 const repeat_password_input_one = document.getElementById('repeat-password-input-one')
@@ -7,3 +7,29 @@ const repeat_password_input_two = document.getElementById('repeat-password-input
 const repeat_password_input_three = document.getElementById('repeat-password-input-three')
 const repeat_password_input_four = document.getElementById('repeat-password-input-four')
 const error_message = document.getElementById('error-message')
+
+form.addEventListener('submit', (e) => {
+    //List of error messages
+    let errors = []
+
+    errors = getSignupFormErrors(
+        firstname_input.value,
+        username.value, password_input.value,
+        repeat_password_input_one.value,
+        repeat_password_input_two,
+        repeat_password_input_three,
+        repeat_password_input_four,
+    )
+
+    if (errors.length > 0) {
+        // If there are any errors
+        e.preventDefault()
+        error_message.innerText = errors.join(". ")
+    }
+})
+
+function getSignupFormErrors () {
+    let errors = [];
+
+
+}
