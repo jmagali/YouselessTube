c8cd33bd2258629aa666106d4b78835e2d2703db
JM <jmagali.dev@gmail.com>
2025-03-16 15:51:11 -0400

    Added the noise game

---
+++ noiseGame.js
@@ -0,0 +1,27 @@
+const volume = document.querySelector("#volume");
+const ball = document.querySelector("#ball");
+
+volume.addEventListener("click", () => {
+
+    let random = Math.random() * ((76) + 60);
+
+    if (random > 100)
+        random = 136;
+
+    document.documentElement.style.setProperty("--xPositionFour", `${random}vh`);
+    document.documentElement.style.setProperty("--xPositionThree", `${random / 4 * 3}vh`);
+    document.documentElement.style.setProperty("--xPositionTwo", `${random / 2}vh`);
+    document.documentElement.style.setProperty("--xPositionOne", `${random / 4}vh`);
+
+    ball.style.animation = 'none';
+
+    ball.offsetHeight;
+
+    ball.style.animation = 'projectile-motion 1s ease-in 0s';
+
+    if (random === 136) {
+        setTimeout(() => {
+            window.location.href = 'videopage.html';
+        }, 1000); 
+    }
+});
